7758d671a524232f9c64570175c999eae13dd58f
DenysKapustianskyi-S <G00470377@atu.ie>
2026-03-04 16:56:46 +0000

    created controller.LoanController

--- src/main/java/ie/atu/oop_inclass_07/service/LoanService.java
+++ src/main/java/ie/atu/oop_inclass_07/service/LoanService.java
@@ -1,19 +1,17 @@
 package ie.atu.oop_inclass_07.service;
 
 import ie.atu.oop_inclass_07.model.Loan;
-import io.swagger.v3.oas.annotations.servers.Server;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Server
+@Getter
+@Service
 
 public class LoanService {
-    private List<Loan> loans = new ArrayList<>();
-
-    public List<Loan> getLoans() {
-        return loans;
-    }
+    private final List<Loan> loans = new ArrayList<>();
 
     public Loan addLoan(Loan loan) {
         loans.add(loan);
